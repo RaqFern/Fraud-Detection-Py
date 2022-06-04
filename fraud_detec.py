68c2c2ed0ace8c4fcff3476728407260bb80aebf
Raquel Fernandes <rq.fernandes@hotmail.com>
2022-06-04 20:42:23 -0300

    Creating File

---
+++ fraud_detec.py
@@ -0,0 +1,13 @@
+# importing libs and dataset
+import pandas as pd
+import numpy as np
+data = pd.read_csv("fraud_detection.csv")
+print(data.head())
+
+# null values? let's check
+print(data.isnull().sum())
+
+# This data does not have any null values, great! moving on...
+
+# Exploring transaction type
+print(data.type.value_counts())
